e374c41bd6e3627a1789878f84672e0e09fe1e42
Damian Tymkowski <thedamianpl@gmail.com>
2021-03-31 17:13:00 +0200

    Add change username functionality to the controller

--- src/Controller/SettingsController.php
+++ src/Controller/SettingsController.php
@@ -1,8 +1,10 @@
 <?php
 namespace App\Controller;
 use App\Entity\ChangePassword;
+use App\Entity\ChangeUsername;
 use App\Entity\User;
 use App\Form\ChangePasswordFormType;
+use App\Form\ChangeUsernameFormType;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,37 +17,50 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class SettingsController extends AbstractController
 {
     /**
-     * @Route("/admin/password/change", name="admin_password_change")
+     * @Route("/admin/settings", name="admin_settings")
      * @IsGranted("ROLE_ADMIN")
      * @param Request $request
      * @param UserPasswordEncoderInterface $passwordEncoder
      * @return Response
      */
-    public function changePassword(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
 
         $changePasswordModel = new ChangePassword();
-        $form = $this->createForm(ChangePasswordFormType::class, $changePasswordModel);
+        $form_changePassword = $this->createForm(ChangePasswordFormType::class, $changePasswordModel);
 
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
+        $form_changePassword->handleRequest($request);
+        if($form_changePassword->isSubmitted() && $form_changePassword->isValid()){
             $entityManager = $this->getDoctrine()->getManager();
             $user = $entityManager->find(User::class, $this->getUser()->getId());
             $user->setPassword(
                 $passwordEncoder->encodePassword(
                     $user,
-                    $form->get('newPassword')->getData()
+                    $form_changePassword->get('newPassword')->getData()
                 )
             );
             $entityManager->persist($user);
             $entityManager->flush();
-            return $this->redirect('/admin/password/change?valid=success');
+            return $this->redirect('/admin/settings?changePassword=success');
         }
 
+        $changeUsernameModel = new ChangeUsername();
+        $form_changeUsername = $this->createForm(ChangeUsernameFormType::class, $changeUsernameModel);
+        $form_changeUsername->handleRequest($request);
+        if($form_changeUsername->isSubmitted() && $form_changeUsername->isValid()){
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->find(User::class, $this->getUser()->getId());
+            $user->setUsername( $form_changeUsername->get('newUsername')->getData());
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirect('/admin/settings?changeUsername=success');
+        }
 
-        return $this->render('settings/change-password.html.twig',[
-           'changePasswordForm' => $form->createView(),
+        return $this->render('settings/settings.twig', [
+            'changeUsernameForm'=>$form_changeUsername->createView(),
+            'changePasswordForm'=>$form_changePassword->createView(),
         ]);
     }
+
 
 }
